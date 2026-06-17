2a6c40078a1b191201d093d95cfe61ac9b5188be
Alexandr Nefedov <nefedov19911991@gmail.com>
2026-06-17 09:22:49 +0300

    Changes names of variables and change function

--- Dogs.py
+++ Dogs.py
@@ -48,24 +48,24 @@ def write_to_json(new_list):
             json.dump(file_data, file)
 
 #создаем папку dogs
-def create_main_folder(token):
+def create_main_folder(token, path):
     headers = {
         'Authorization' : f'OAuth {token}'
     }
     params = {
-        'path' : 'dogs'
+        'path' : path
     }
     response = requests.put('https://cloud-api.yandex.net/v1/disk/resources', headers = headers, params = params)
 
 def get_pictures_url_subbreed(breed):
-    response2 = get_list_of_subbread(breed)
-    list_of_types = []
-    if response2:
-        for item in response2["message"]:
-            url3 = f'https://dog.ceo/api/breed/{breed}/{item}/images'
-            response3 = get_one_picture_subbread(url3)
-            list_of_types.append(response3['message'][0])
-    return list_of_types
+    response = get_list_of_subbread(breed)
+    types = []
+    if response:
+        for item in response["message"]:
+            url = f'https://dog.ceo/api/breed/{breed}/{item}/images'
+            response2 = get_one_picture_subbread(url)
+            types.append(response2['message'][0])
+    return types
 
 def get_list_of_subbread(breed):
     url = f'https://dog.ceo/api/breed/{breed}/list'
@@ -76,43 +76,43 @@ def get_one_picture_subbread(url):
     response = requests.get(url).json()
     return response
 
-def get_file_names(list_of_types):
-    list_of_filenames =[]
-    for item in list_of_types:
-        x = item.split('/')
-        y = x[-2] +'_'+ x[-1]
-        list_of_filenames.append(y)
-    return list_of_filenames
+def get_file_names(types):
+    filenames =[]
+    for item in types:
+        part_url = item.split('/')
+        name = part_url[-2] +'_'+ part_url[-1]
+        filenames.append(name)
+    return filenames
 
 def get_pictures_url_breed(breed):
-    list_of_types = []
+    types = []
     print('Нет подпород')
     url = f'https://dog.ceo/api/breed/{breed}/images/random/3'
     response = requests.get(url).json()
     for item in response['message']:
-        list_of_types.append(item)
-    return list_of_types
+        types.append(item)
+    return types
 
 breed = input('Введите породу собаки (на английском языке): ')
-create_main_folder(token)
+create_main_folder(token, 'dogs')
 
-list_of_subbreed = get_pictures_url_subbreed(breed)
-names_of_subbreed = get_file_names(list_of_subbreed)
-if list_of_subbreed != []:
+subbreed = get_pictures_url_subbreed(breed)
+names = get_file_names(subbreed)
+if subbreed != []:
     create_internal_folder_yadisk(token, breed)
     counter = 0
-    while counter < len(list_of_subbreed):
-        load_picture_yadisk(list_of_subbreed[counter], names_of_subbreed[counter], token, breed)
+    while counter < len(subbreed):
+        load_picture_yadisk(subbreed[counter], names[counter], token, breed)
         counter = counter + 1
-    write_to_json(list_of_subbreed)
+    write_to_json(subbreed)
 else:
-    list_of_breed = get_pictures_url_breed(breed)
-    names_of_breed = get_file_names(list_of_breed)
+    breed_pic = get_pictures_url_breed(breed)
+    names = get_file_names(breed_pic)
     create_internal_folder_yadisk(token, breed)
     counter = 0
-    while counter < len(list_of_breed):
-        load_picture_yadisk(list_of_breed[counter], names_of_breed[counter], token, breed)
+    while counter < len(breed_pic):
+        load_picture_yadisk(breed_pic[counter], names[counter], token, breed)
         counter = counter + 1
-    write_to_json(list_of_breed)
+    write_to_json(breed_pic)
 
 
